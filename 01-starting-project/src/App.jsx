0bc7818027ea5cdc2d6699e01ffb98dd7d83f2ab
yyubin <hazing120@gmail.com>
2024-03-27 08:10:27 +0900

    props syntaxes

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,11 +22,28 @@ function Header() {
   );
 }
 
+function CoreConcept({image, title, description}) {
+  return <li>
+    <img src={image}/>
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
+
 function App() {
   return (
     <div>
       <main>
-        <Header />
+      <Header />
+        <section id="core-concepts">
+          <h2>Core Concpets</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
