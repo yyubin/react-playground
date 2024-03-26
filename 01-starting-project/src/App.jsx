b39cd0e2cd3afa65bdbf15b91606e8637ee38a78
yyubin <hazing120@gmail.com>
2024-03-27 07:14:27 +0900

    constantization

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,12 +5,14 @@ function getRandomInt(max) {
 }
 
 function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
