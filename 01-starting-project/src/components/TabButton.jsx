cffa31b2a23f7a9abcd2976b157161b548496ad6
yyubin <hazing120@gmail.com>
2024-04-07 00:15:58 +0900

    reacting to events

---
+++ 01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,12 @@
+export default function TabButton({ children }) {
+    function handleClick() {
+        console.log('Hello World');
+    }
+
+    return (
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
+    );
+
+}
