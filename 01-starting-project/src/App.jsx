db25c39afc25d3db4dcf6c2fcaf2c2f6a111d54c
yyubin <hazing120@gmail.com>
2024-04-07 01:01:52 +0900

    outputting data based on state

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  const [ selectedTopic, setSeletedTopic ] = useState('Please click a button');
+  const [ selectedTopic, setSeletedTopic ] = useState('components');
 
   function handleSelect(selectedButton) {
     // selectedButton => "components", "jsx", "props", "state"
     setSeletedTopic(selectedButton);
-    console.log(selectedButton);
+    console.log(selectedButton); // 이전값 출력 다시 앱을 재실행 하기 전까진 예전 앱이기 때문
   }
 
   return (
@@ -35,7 +35,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
