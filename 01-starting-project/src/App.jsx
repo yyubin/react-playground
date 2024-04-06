ff057647e71c6755c6de426f899671403541caf2
yyubin <hazing120@gmail.com>
2024-04-07 08:08:37 +0900

    forwarding props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-      <main>
       <Header />
+      <main>
         <CoreConcepts />
         <Examples />
       </main>
--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data";
+import Section from "./Section.jsx";
 import TabButton from "./TabButton.jsx";
 
 export default function Examples() {
@@ -24,15 +25,14 @@ export default function Examples() {
     }
 
     return (
-        <section id="examples">
-        <h2>Examples</h2>
+        <Section title="Examples" id="examples">
         <menu>
-          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+          <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
         </menu>
           {tabContent}
-      </section>
+      </Section>
     )
 }
