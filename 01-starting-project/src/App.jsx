e94e3174d8271a0b66ad387000dfe8eb6fd5e4ec
yyubin <hazing120@gmail.com>
2024-04-07 01:14:51 +0900

    conditionally rendering

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -6,12 +6,24 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  const [ selectedTopic, setSeletedTopic ] = useState('components');
+  const [ selectedTopic, setSeletedTopic ] = useState();
 
   function handleSelect(selectedButton) {
     // selectedButton => "components", "jsx", "props", "state"
     setSeletedTopic(selectedButton);
     console.log(selectedButton); // 이전값 출력 다시 앱을 재실행 하기 전까진 예전 앱이기 때문
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+  
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
   }
 
   return (
@@ -35,13 +47,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+            {tabContent}
         </section>
       </main>
     </div>
