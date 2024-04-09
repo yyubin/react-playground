9d4d841cc31cf417e166596917fe15345bcc3612
yyubin <hazing120@gmail.com>
2024-04-09 12:11:35 +0900

    Exercise using state

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 export default function Player({ name, symbol }) {
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleClick() {
+        setIsEditing(!isEditing);
+    }
+
+    let playerContent = <span className="player-name">{name}</span>;
+
+    if(isEditing) {
+        playerContent = <input></input>;
+    }
+
     return (
         <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+            {playerContent}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleClick}>Edit</button>
       </li>
     )
 }
