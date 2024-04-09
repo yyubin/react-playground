ea1d739c41cd0a6b66117de2dcc50e5dba01aa41
yyubin <hazing120@gmail.com>
2024-04-09 12:44:51 +0900

    Bind user input

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react"
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        // setIsEditing(!isEditing); // => schedules a state update to true
+        // setIsEditing(!isEditing); // 둘다 처음 값(false)을 true로 바꾸기만 함
+        setIsEditing((editing) => !editing); // !isEditing은 상태 업데이트가 즉시 실행되지 않는 문제가 있음 
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let eidtablePlayerName = <span className="player-name">{playerName}</span>;
 
     if(isEditing) {
-        playerName = <input></input>;
+        eidtablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     }
 
     return (
         <li>
         <span className="player">
-            {playerName}
+            {eidtablePlayerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing? 'Save' : 'Edit'}</button>
       </li>
     )
 }
