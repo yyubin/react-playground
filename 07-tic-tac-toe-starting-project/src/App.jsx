4a8ae444f46e738077bf007366fa10dbba4e6c5d
yyubin <hazing120@gmail.com>
2024-04-09 12:01:44 +0900

    Spit components

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,18 @@
+import Player from "./components/Player"
 
 function App() {
   
 
   return (
-    <p>Coming soon..</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+
+      </div>
+    </main>
   )
 }
 
