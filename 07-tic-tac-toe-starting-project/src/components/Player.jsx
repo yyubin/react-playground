87273f72d18cd7ea7a2a4164e162427292143ac6
yyubin <hazing120@gmail.com>
2024-04-09 12:16:25 +0900

    Check exercise

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,23 +3,23 @@ import { useState } from "react"
 export default function Player({ name, symbol }) {
     const [ isEditing, setIsEditing ] = useState(false);
 
-    function handleClick() {
+    function handleEditClick() {
         setIsEditing(!isEditing);
     }
 
-    let playerContent = <span className="player-name">{name}</span>;
+    let playerName = <span className="player-name">{name}</span>;
 
     if(isEditing) {
-        playerContent = <input></input>;
+        playerName = <input></input>;
     }
 
     return (
         <li>
         <span className="player">
-            {playerContent}
+            {playerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleClick}>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
       </li>
     )
 }
