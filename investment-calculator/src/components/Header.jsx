c5272a8be4c970500ccad6701dcbe76cb2dc3d81
yyubin <hazing120@gmail.com>
2024-04-11 05:15:14 +0900

    Add header

---
+++ investment-calculator/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div id="header">
+            <img src="investment-calculator-logo.png" />
+            <h1>React Investment Calculator</h1>
+        </div>
+    )
+}
