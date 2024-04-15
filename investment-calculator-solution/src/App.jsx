9c8f35d405affb321340cf1242f2421c97f7cc7b
yyubin <hazing120@gmail.com>
2024-04-15 13:02:24 +0900

    Render conditionally

--- investment-calculator-solution/src/App.jsx
+++ investment-calculator-solution/src/App.jsx
@@ -20,11 +20,14 @@ function App() {
     });
 }
 
+const inputIsValid = userInput.duration >= 1;
+
   return (
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className='center'>Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   )
 }
